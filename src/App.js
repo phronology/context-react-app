1866f5e9b9191d6eb926840b7cea9ad8a49e89f2
phronology <luke.mcmanus@digi2al.co.uk>
2020-08-22 14:33:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,127 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {Table, Button} from  'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsDown , faThumbsUp , faImage , faMoneyCheckAlt, faSearchDollar } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = { 
+        isLoading :false,
+        invoices :  [
+            // {
+            //     "id" : "100",
+            //     "Vendor":"Hankook",
+            //     "Amount" : "$18,000",
+            //     "invoice" : "1123",
+            //     "Date" : "08/21/2019"
+            // },
+
+            // {
+            //     "id" : "200",
+            //     "Vendor":"Hankook",
+            //     "Amount" : "$18,000",
+            //     "invoice" : "1123",
+            //     "Date" : "08/21/2019"
+            // },
+
+            // {
+            //     "id" : "300",
+            //     "Vendor":"Hankook",
+            //     "Amount" : "$18,000",
+            //     "invoice" : "1123",
+            //     "Date" : "08/21/2019"
+            // }
+        ]
+     }
+
+    remove(id){
+        console.log(id);
+        let updateedInvoices = [...this.state.invoices].filter (i => i.id !== id)
+
+        this.setState({invoices : updateedInvoices});
+    }
+
+
+    async componentDidMount() {
+        const response = await fetch(
+          "https://mgbpcdtxkb.execute-api.eu-west-2.amazonaws.com/dev"
+        );
+        const body = await response.json();
+        this.setState({ invoices: body, isLoading: false });
+    }
+    
+      
+
+    render() { 
+
+        const isLoading = this.state.isLoading;
+        const allinvoices = this.state.invoices;
+
+
+
+        if (isLoading)
+            return(<div>Loading...</div>);
+
+
+        let invoices = 
+        allinvoices.map( invoice => 
+            <tr key={invoice.email}>
+                <td>{invoice.reference}</td>
+                <td>{invoice.phone}</td>
+                <td>{invoice.environment}</td>
+                <td>{invoice.dob}</td>
+                <td><Button className="btn btn-lg btn-success" onClick={ () => this.remove(invoice.email)} > <FontAwesomeIcon icon={faThumbsUp} /> OK </Button></td>
+                <td><Button className="btn btn-lg btn-danger" onClick={ () => this.remove(invoice.email)} > <FontAwesomeIcon icon={faThumbsDown} /> NOK </Button></td>
+                <td><Button className="btn btn-lg btn-info" onClick={ () => this.remove(invoice.email)} > <FontAwesomeIcon icon={faMoneyCheckAlt} /> 50% </Button></td>
+                <td><Button className="btn btn-lg btn-warning" onClick={ () => this.remove(invoice.email)} ><FontAwesomeIcon icon={faSearchDollar} /> ?? </Button></td>
+                <td><Button className="btn btn-lg btn-info" onClick={ () => this.remove(invoice.email)} > <FontAwesomeIcon icon={faImage} /> Image </Button></td>
+            </tr>
+        )
+
+        return (
+            
+            <div className="container border border-secondary rouded center">
+
+                <div className="row">
+                        <div className="col-12">
+                            <h4>Pending Invoices - The Test Comapny</h4>
+                        </div>
+                </div>
+
+                <div className="row">
+                        <div className=".col-xs-12 center text-center">
+                            <Table dark responsive striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th >Vendor</th>
+                                        <th>Amount</th>
+                                        <th>Invoice #</th>
+                                        <th>Date</th>
+                                        <th colSpan="4">Actions</th>
+                                        <th>Image</th>
+                                    </tr>
+                                </thead>
+                            
+                            <tbody>
+                                {this.state.invoices.length === 0 ? <td colSpan="9">All caught up!</td> : invoices}
+                            </tbody>
+                            </Table>
+
+                        </div>
+
+                </div>
+
+
+
+            </div>
+
+        );
+    }
 }
-
+ 
 export default App;
+
+
+
+
+

487ec26265a1975e9403233ce95812d29436647c
phronology <luke.mcmanus@digi2al.co.uk>
2020-08-27 18:07:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Table, Button} from  'reactstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faThumbsDown , faThumbsUp , faImage , faMoneyCheckAlt, faSearchDollar } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faThumbsDown , faThumbsUp , faImage , faMoneyCheckAlt, faSearchDollar } from '@fortawesome/free-solid-svg-icons'
 
 class App extends Component {
     constructor(props) {
@@ -13,13 +13,8 @@ class App extends Component {
         console.log(this.state.invoices)
         this.handleInputChange = this.handleInputChange.bind(this)
         this.deletePerson = this.deletePerson.bind(this);
-
-    
     }
-    // state = { 
-    //     isLoading :false,
-    //     invoices :  []
-    //  }
+    
 
     remove(id){
         console.log(id);
@@ -29,26 +24,17 @@ class App extends Component {
     }
 
 
-    // async componentDidMount() {
-    //     const response = await fetch(
-    //       "https://mgbpcdtxkb.execute-api.eu-west-2.amazonaws.com/dev"
-    //     );
-    //     const body = await response.json();
-    //     this.setState({ invoices: body, isLoading: false });
-    //     console.log(this.state.invoices)
-    // }
 
     componentDidMount() {
         this.getPeople();
     }
+
 
     getPeople() {
         fetch("https://mgbpcdtxkb.execute-api.eu-west-2.amazonaws.com/dev")
         .then(response => response.json())
         .then(response => this.setState({ invoices: response }))
         .catch(error => console.log(error));
-        
-
     };
     
     
@@ -56,105 +42,40 @@ class App extends Component {
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const email_addr = target.id
-        let k = "email"
+        const n = target.name
+        console.log(n)
         const copy = this.state.invoices
-        // console.log(copy[1])
         const found = copy.findIndex(element => element.email == email_addr)
-        console.log(copy)
-        console.log(found)
-        // for (let x in this.state.invoices){
-        //     console.log(typeof(x))
-        // }
-        console.log(value)
-        console.log(this.state)
+       
         
-
-
-        // this.setState({
-        //     invoices: [
-        //         ...this.state.invoices,
-        //         this.state.invoices[0].manual_approval = value
-                        
-               
-        //     ],
-        //   });
-
-        
-        // let key = "email";
-        // console.log(key)
-        // this.setState(prevState => ({
-          
-        //     invoices: prevState.invoices.map(
-        //         el => el.key = email ? { ...el, email: email, manual_approval: value } : el
-
-        //     )
-              
-          
-        // }
-        // ))
 
         this.setState(prevState => {
-            // let idx = 
-            let jasper = Object.assign({}, prevState.invoices);  // creating copy of state variable jasper
-            jasper[found].manual_approval = value;                     // update the name property, assign a new value                 
-            return { jasper };                                 // return new object jasper object
+            let inv = Object.assign({}, prevState.invoices);  
+            inv[found][n] = value;              // manual_approval needs changing to a variable.. a key?                       
+            return { inv };                                 
           })
 
-        // let key = "email";
-        // console.log(key)
-
-        // this.setState(prevState => ({
-            
-        //     invoices: prevState.invoices.findIndex(
-        //         // el => el.key === email ? { ...el, email: email, manual_approval: value } : el
-                
-        //         el => el.key = email ? { ...el, manual_approval: value } : el
-        //     )
-              
-          
-        // }
-        // ))
-     
         
-
-
+     
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            // body: JSON.stringify({ title: 'React POST Request Example' })
             body: JSON.stringify({
                 "operation": "update",
                 "tableName": "context-checks-applications",
                 "payload": {
                     "Item": {
-                        "email": email_addr,
-                        "manual_approval": value
+                        "email": email_addr, // neccesarry for DB PK
+                        [n] : value
                     }
                 }
             })
         };
         console.log(requestOptions)
         fetch('https://mgbpcdtxkb.execute-api.eu-west-2.amazonaws.com/dev', requestOptions)
-        // .then(response => response.json())
-        // .then(data => this.setState({ postId: data.id }));
-
-        // this.setState({invoice : true})
-
-        // this.setState(filter())
-
-        // this.setState({ choice: value })
-        // console.log(`onInput: ${this.state.choice}`)
-
-        // this.setState(prevState =>{
-        //     return{
-        //          console.log(...prevState)
-        //     }
-        //  })
-        
-    
-        
     }
     
+
     deletePerson(email) {
         return () => {
           this.setState(prevState => ({
@@ -163,41 +84,37 @@ class App extends Component {
         };
       }
     
-    // deletePerson(manual_approval) {
-    // return () => {
-    //     this.setState(prevState => ({
-    //     invoices: prevState.invoices.manual_approval // filtering out row by email
-    //     }));
-    // };
-    // }
-
+   
     render() { 
-
         const isLoading = this.state.isLoading;
         const allinvoices = this.state.invoices;
-
-        console.log(allinvoices)
 
         if (isLoading)
             return(<div>Loading...</div>);
 
-        
         let invoices = 
         allinvoices.map( invoice => 
             <tr key={invoice.email}>
-                {/* <td>{invoice.}</td> */}
                 <td>
                     <form>
                         <input
                         type="checkbox"
-                        id={invoice.email} checked={invoice.manual_approval} // is it getting stuck in a checkbox = checked loop?
+                        id={invoice.email} checked={invoice.manual_approval} name="manual_approval" 
                         // value={invoice.manual_approval}
                         onChange={this.handleInputChange}
                         />
                     </form>
-                    {/* <Button className="btn btn-sm btn-success" onClick={this.deletePerson(invoice.email)} > <FontAwesomeIcon icon={faThumbsUp} /> Save </Button> */}
                 </td>
-                {/* <td>{invoice.email}</td> */}
+                <td>
+                    <form>
+                        <input
+                        type="checkbox"
+                        id={invoice.email} checked={invoice.bolt_ons} name="bolt_ons" 
+                        // value={invoice.manual_approval}
+                        onChange={this.handleInputChange}
+                        />
+                    </form>
+                </td>
                 <td>{invoice.email}</td>
                 <td>{invoice.reference}</td>
                 <td>{invoice.environment}</td>
@@ -215,30 +132,24 @@ class App extends Component {
                 <td>{invoice.created}</td>
                 <td>{invoice.completed}</td>
                 <td>{invoice.result}</td>
-                {/* <td><Button className="btn btn-lg btn-success" onClick={ () => this.remove(invoice.email)} > <FontAwesomeIcon icon={faThumbsUp} /> OK </Button></td>
-                <td><Button className="btn btn-lg btn-danger" onClick={ () => this.remove(invoice.email)} > <FontAwesomeIcon icon={faThumbsDown} /> NOK </Button></td>
-                <td><Button className="btn btn-lg btn-info" onClick={ () => this.remove(invoice.email)} > <FontAwesomeIcon icon={faMoneyCheckAlt} /> 50% </Button></td>
-                <td><Button className="btn btn-lg btn-warning" onClick={ () => this.remove(invoice.email)} ><FontAwesomeIcon icon={faSearchDollar} /> ?? </Button></td>
-                <td><Button className="btn btn-lg btn-info" onClick={ () => this.remove(invoice.email)} > <FontAwesomeIcon icon={faImage} /> Image </Button></td> */}
+                {/* <Button className="btn btn-sm btn-success" onClick={this.deletePerson(invoice.email)} > <FontAwesomeIcon icon={faThumbsUp} /> Save </Button> */}
             </tr>
         )
 
         return (
-            
             <div className="container border-secondary rouded center">
-
                 <div className="row">
                         <div className="col-12">
                             <h4>Application Tracker</h4>
                         </div>
                 </div>
-
                 <div className="row">
                         <div className=".col-xs-12 center text-center">
                             <Table dark responsive striped bordered hover>
                                 <thead>
                                     <tr>
                                         <th>Manual Approval</th>
+                                        <th>Bolt-on Checks</th>
                                         <th>Email</th>
                                         <th>Reference</th>
                                         <th>Environment</th>
@@ -260,20 +171,13 @@ class App extends Component {
                                         {/* <th>Image</th> */}
                                     </tr>
                                 </thead>
-                            
                             <tbody>
                                 {this.state.invoices.length === 0 ? <td colSpan="9">All caught up!</td> : invoices}
                             </tbody>
                             </Table>
-
                         </div>
-
                 </div>
-
-
-
             </div>
-
         );
     }
 }
